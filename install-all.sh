91566e621bc3def58d43039934fca8c488f04984
Mitchell Williams <twhiterabbite@gmail.com>
2020-10-13 13:52:54 -0400

    Correct and add additional elements for installation

--- install-all.sh
+++ install-all.sh
@@ -5,7 +5,7 @@ echo "Installing Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Installing Cask..."
-brew tap caskroom/cask
+brew tap homebrew/cask
 
 # needs
 brew install trash-cli
@@ -16,12 +16,14 @@ brew install docker
 
 brew install zsh-autosuggestions zsh-syntax-highlighting
 
+brew install zsh
 brew install z
 brew install ag
 brew install fzf
 brew install bat
 
-brew install vim --override-system-vim
+brew install vim
+brew link vim # link to override the systems installed vim
 brew install neovim
 
 brew install kubectl
@@ -38,6 +40,9 @@ brew cask install google-chrome
 brew cask install slack
 brew cask install spotify
 brew cask install alfred
+brew cask install 1password
+brew cask install hazel
+brew cask install dash
 
 # Clone dot files?
 
